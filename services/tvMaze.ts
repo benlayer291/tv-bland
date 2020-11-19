f1259495d2ef135abd8b7a6d75dd4551be7ba4f1
Ben Layer <benlayer291@gmail.com>
2020-11-19 22:05:05 +0000

    Setup service for fetching from tvMaze Api

---
+++ services/tvMaze.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+import { InterfaceTvSchedule, InterfaceTvShow } from '../types/interfaces'
+
+const axiosClient = axios.create({
+  baseURL: 'https://api.tvmaze.com/',
+})
+
+export const fetchSchedule = async (
+  date?: string
+): Promise<InterfaceTvSchedule[]> => {
+  const url = date ? `schedule?country=GBdate=${date}` : `schedule?country=GB`
+  const { data } = await axiosClient({
+    url,
+    method: 'GET',
+  })
+
+  return data
+}
+
+export const fetchShow = async (id: string): Promise<InterfaceTvShow> => {
+  const url = `shows/${id}?embed=cast`
+  const { data } = await axiosClient({
+    url,
+    method: 'GET',
+  })
+
+  return data
+}
