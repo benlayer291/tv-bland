3acbdbe5ea1e801ca4169e7187f11062dea15323
Ben Layer <benlayer291@gmail.com>
2020-11-23 22:43:42 +0000

    Feature/lighthouse (#10)
    * Add lang attribute to html
    
    * Increase user scalability threshold
    
    * Allow user scaling

--- components/theLayout/theLayout.tsx
+++ components/theLayout/theLayout.tsx
@@ -16,7 +16,7 @@ const TheLayout: React.FunctionComponent<Props> = ({ children }: Props) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
         />
         <meta
           name="description"
