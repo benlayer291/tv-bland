716a84bdf4d9eed34a4d84c6a0f917aecf8a9fa1
Ben Layer <benlayer291@gmail.com>
2020-11-23 15:06:46 +0000

    Try route change css temp fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Router from 'next/router'
 import { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
 import { AnimatePresence } from 'framer-motion'
@@ -9,10 +10,28 @@ const TheCursor = dynamic(() => import('../components/theCursor/theCursor'), {
 
 import '../styles/globals.css'
 
+const routeChange = () => {
+  // Temporary fix to avoid flash of unstyled content
+  // during route transitions. Keep an eye on this
+  // issue and remove this code when resolved:
+  // https://github.com/vercel/next.js/issues/17464
+
+  const tempFix = () => {
+    const allStyleElems = document.querySelectorAll('style[media="x"]')
+    allStyleElems.forEach((elem) => {
+      elem.removeAttribute('media')
+    })
+  }
+  tempFix()
+}
+
+Router.events.on('routeChangeComplete', routeChange)
+Router.events.on('routeChangeStart', routeChange)
+
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <>
-      {/* <TheCursor /> */}
+      <TheCursor />
       <TheLayout>
         <AnimatePresence initial={false} exitBeforeEnter>
           <Component {...pageProps} key={router.route} />
